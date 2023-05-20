8dc95b774de900f21712c3abac2619beda1b3e46
rafni <alberto.rcdb@gmail.com>
2023-05-20 18:50:38 +0200

    Migraciones y modelos necesarios

---
+++ app/Models/Lead.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Lead extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'score'
+    ];
+
+    public function client() : HasOne
+    {
+        return $this->hasOne(Client::class);
+    }
+}
